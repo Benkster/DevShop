ebfc27e6791366a8a6a688c8b3afb7c6f6c75ca5
benni <benjamin.knoll.bk@gmail.com>
2022-10-08 11:59:56 +0200

    Created DB-Design

---
+++ DevShop.App/DevShop/Data/DevShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevShop.Data
+{
+	public class DevShopDbContext : IdentityDbContext
+	{
+		public DevShopDbContext(DbContextOptions<DevShopDbContext> options)
+			: base(options)
+		{
+
+		}
+	}
+}
