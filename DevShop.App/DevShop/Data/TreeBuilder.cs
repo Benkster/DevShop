0331202b93efc3b33e370926171ec461c57a164e
benni <benjamin.knoll.bk@gmail.com>
2022-12-04 14:15:03 +0100

    Finish Category-Views

--- DevShop.App/DevShop/Data/TreeBuilder.cs
+++ DevShop.App/DevShop/Data/TreeBuilder.cs
@@ -1,4 +1,7 @@
 ﻿using DevShop.Data.ViewModels.TreeBuilderVMs;
+using Microsoft.AspNetCore.Components;
+using System.Drawing.Imaging;
+using System.Text;
 
 namespace DevShop.Data
 {
@@ -10,15 +13,28 @@ namespace DevShop.Data
     public class TreeBuilder
     {
         #region Variables
+        #region Exclude Children From Selection
         // Contains all child-elements, that are beeing excluded from a select-list
         private List<SelectListDataVM> excludeElemList;
         #endregion
 
 
 
-        #region Constructors
-        public TreeBuilder()
+        #region Build TreeView
+        // Keeps track of the current depth-level while generating the Code for the TreeView
+        int treeDepth = 0;
+
+        // Stores the HTML-Code for a TreeView as string 
+        private StringBuilder treeViewResultHtmlStringBuilder;
+        #endregion
+		#endregion
+
+
+
+		#region Constructors
+		public TreeBuilder()
         {
+            treeViewResultHtmlStringBuilder = new StringBuilder();
             excludeElemList = new List<SelectListDataVM>();
         }
         #endregion
@@ -26,7 +42,7 @@ namespace DevShop.Data
 
 
         #region Methods
-        #region Public Methods
+        #region Exclude Children From Selection
         /// <summary>
         /// Exclude all own child-elements of a specified element from a select-list.
         /// (i.e. if there's a possibility to select a parent-element in an edit-view, the user should not be able to change the parent-element to one of the own child elements)
@@ -62,11 +78,9 @@ namespace DevShop.Data
 
             return resultList;
         }
-        #endregion
 
 
 
-        #region Private Methods
         /// <summary>
         /// This recursive method adds all child-elements, that should be excluded from a selection, to a list
         /// </summary>
@@ -93,6 +107,124 @@ namespace DevShop.Data
                 }
             }
         }
+		#endregion
+
+
+
+		#region Build TreeView
+		/// <summary>
+		/// Create HTML-Code for a TreeView (nested ul and li-elements) out of a list of elements
+		/// </summary>
+		/// <param name="_treeViewElements">
+		/// A list of elements, that should be displayed in the TreeView
+		/// </param>
+		/// <returns>
+		/// A MarkupString containing the HTML-Code of the TreeView
+		/// </returns>
+		public MarkupString BuildTree(List<TreeViewElementVM> _treeViewElements)
+        {
+            // Result HTML as string
+            string treeViewResultHtml = string.Empty;
+            // Used to build up the string for the TreeView
+            treeViewResultHtmlStringBuilder = new StringBuilder();
+
+            treeDepth = 0;
+
+            // Sort the list after ParentID and ID of the elements
+            _treeViewElements = _treeViewElements.OrderBy(t => t.ParentID).OrderBy(t => t.ElemID).ToList();
+
+
+            // Generate HTML-Code for the TreeView as string
+            BuildTreeHtml(_treeViewElements, _treeViewElements.Where(t => t.ParentID == 0).ToList());
+
+            treeViewResultHtml = treeViewResultHtmlStringBuilder.ToString();
+
+
+
+            // Return the HTML-Code as MarkupString (makes it possible to display the string as actual HTML-elements in the view)
+            return (MarkupString)treeViewResultHtml;
+        }
+
+
+
+        /// <summary>
+        /// Recursively go through a list of elements to build up a TreeView with ul-li elements.
+        /// HTML-Code is stored as string.
+        /// </summary>
+        /// <param name="_fullList">
+        /// A list containing all existing elements, that should be shown in the TreeView
+        /// </param>
+        /// <param name="_treeViewLevelElements">
+        /// A list of all elements of the current depth-level of the TreeView (i.e. direct child-elements of another element)
+        /// </param>
+        private void BuildTreeHtml(List<TreeViewElementVM> _fullList, List<TreeViewElementVM> _treeViewLevelElements)
+		{
+            // Decend down to next depth-level of TreeView
+            treeDepth++;
+
+
+            // Add a radio-button and label for the sub-menu (needed to open it on mobile-devices)
+            if (treeDepth > 1)
+			{
+                treeViewResultHtmlStringBuilder.Append(
+                    "<input id=\"rad_tree_openSub_" + _treeViewLevelElements.FirstOrDefault().ParentID.ToString() + "\" class=\"rad_tree_openSub\" type=\"radio\" name=\"rad_tree_openSub\">" +
+                    "<label id=\"lbl_tree_openSub_" + _treeViewLevelElements.FirstOrDefault().ParentID.ToString() + "\" class=\"lbl_tree_openSub\" for=\"rad_tree_openSub_" + _treeViewLevelElements.FirstOrDefault().ParentID.ToString() + "\"></label>"
+                );
+			}
+            // Before the TreeView begins, add radio-buttons and a label to be able to open it on mobile-devices
+            else
+			{
+                treeViewResultHtmlStringBuilder.Append(
+                    "<input id=\"rad_tree_closeSub\" type=\"radio\" name=\"rad_tree_openSub\">" +
+                    "<input id=\"chk_toggleTree\" type=\"checkbox\">" +
+                    "<label id=\"lbl_openTree\" class=\"lbl_toggleTree\" for=\"chk_toggleTree\"></label>" +
+                    "<label id=\"lbl_closeTree_layerBack\" for=\"chk_toggleTree\"></label>"
+                );
+			}
+
+
+            // Open the ul-element
+            treeViewResultHtmlStringBuilder.Append("<ul id=\"treeLayer_" + treeDepth.ToString() + "\">");
+
+
+            // Add a label to close the sub-menu (on mobile devices)
+            if (treeDepth > 1)
+			{
+                treeViewResultHtmlStringBuilder.Append("<label id=\"lbl_tree_closeSub_" + _treeViewLevelElements.FirstOrDefault().ParentID.ToString() + "\" class=\"lbl_tree_closeSub\" for=\"rad_tree_closeSub\"></label>");
+			}
+            // Add a label to close the TreeView (on mobile devices)
+            else
+			{
+                treeViewResultHtmlStringBuilder.Append("<label id=\"lbl_closeTree\" class=\"lbl_toggleTree\" for=\"chk_toggleTree\"></label>");
+			}
+
+
+            // Loop through all direct children of the current element
+            foreach (TreeViewElementVM elem in _treeViewLevelElements)
+			{
+                // Add an li-element containing a link with the name of the current element
+                treeViewResultHtmlStringBuilder.Append(
+                    "<li class=\"treeItem treeItemLayer_" + treeDepth.ToString() + "\">" +
+                    "<a class=\"treeLink\" href=\"" + elem.ElemLink + "\">" + elem.ElemText + "</a>"
+                );
+
+
+                // If the current child-element also has children, add them to the TreeView as well
+                if (_fullList.Any(f => f.ParentID == elem.ElemID))
+                {
+                    BuildTreeHtml(_fullList, _fullList.Where(f => f.ParentID == elem.ElemID).ToList());
+                }
+
+
+                treeViewResultHtmlStringBuilder.Append("</li>");
+			}
+
+            // Close the ul-element
+            treeViewResultHtmlStringBuilder.Append("</ul>");
+
+            // Climb up one depth-level of the TreeView
+            treeDepth--;
+		}
         #endregion
         #endregion
     }
--- DevShop.App/DevShop/Pages/Views/Categories/CategoryEditView.cs
+++ DevShop.App/DevShop/Pages/Views/Categories/CategoryEditView.cs
@@ -1,24 +1,39 @@
 ﻿using DevShop.Data.Models;
 using DevShop.Data.SortTypes;
+using DevShop.Data.ViewModels.TreeBuilderVMs;
 using Microsoft.AspNetCore.Components;
+using System.Drawing.Text;
 
 namespace DevShop.Pages.Views.Categories
 {
+    /// <summary>
+    /// Class for the Category-Edit-View.
+    /// Contains methods and variables, that are necessary for the View or for retrieving data from the Repository
+    /// </summary>
     public partial class CategoryEditView
     {
         #region Variables/Properties
+        // Determines, whether an existing model is beeing edited or a new one is beeing created
         private bool isEdit = false;
 
+        // Parent-ID of the model (from select-element)
         private int parentID = 0;
 
+        // Displays an error message, if something went wrong
         private string errorMessage = string.Empty;
 
+        // Holds the HTML-Code for the TreeView
+        MarkupString treeViewMarkup;
+
+        // Used as a model for the edit-form
         private Category category;
 
-        private List<Category> categories;
+        // List of categories, that are allowed to be selected as parent-element
+        private List<Category> formCategories;
 
 
 
+        // ID of the category, that is beeing edited
         [Parameter]
         public string CategoryID { get; set; }
 		#endregion
@@ -27,43 +42,95 @@ namespace DevShop.Pages.Views.Categories
 
 		#region Methods
 		#region Main-Methods
-		protected override async Task OnInitializedAsync()
+        /// <summary>
+        /// Called, when the parameters of the URL change.
+        /// This is needed instead of the OnInitializedAsync, because when the user selects something in the TreeView (which changes the parameters of the URL), the displayed data in the form has to update.
+        /// </summary>
+		protected override async Task OnParametersSetAsync()
         {
+            // An existing category is beeing edited
             if (!string.IsNullOrEmpty(CategoryID))
             {
                 isEdit = true;
                 category = await uow.CategoryRepo.GetModelByPkAsync(Convert.ToInt32(CategoryID));
-                categories = await uow.CategoryRepo.GetModelsWithoutChildrenAsync(Convert.ToInt32(CategoryID));
+                formCategories = await uow.CategoryRepo.GetModelsWithoutChildrenAsync(Convert.ToInt32(CategoryID));
             }
+            // A new category is beeing created
             else
             {
                 category = new Category();
-                categories = await uow.CategoryRepo.GetAllModelsAsync(CategorySortType.SortType.Name);
+                formCategories = await uow.CategoryRepo.GetAllModelsAsync(CategorySortType.SortType.Name);
             }
-        }
+
+
+            // Only genereate the TreeView, if it has not yet been done (=> minimize traffic and loading time when changing the selected category in the TreeView)
+            if (string.IsNullOrEmpty(treeViewMarkup.Value))
+            {
+                GenerateTreeView();
+            }
+
+
+            // Update the data on the view
+            StateHasChanged();
+		}
 
 
 
+        /// <summary>
+        /// Saves a new category or updates an existing one
+        /// </summary>
         private async Task Save()
         {
+            // Set the selected parent-element
             category.ParentId = parentID;
 
 
+            // Update an existing category
             if (isEdit)
             {
                 await uow.CategoryRepo.UpdateModelAsync(category);
+
+                // Parent-element might have changed, so the TreeView must be updated
+                GenerateTreeView();
             }
+            // Create a new category
             else
             {
                 await uow.CategoryRepo.CreateModelAsync(category);
+                nav.NavigateTo("/category");
             }
-
-
-            nav.NavigateTo("/category");
         }
 
 
 
+        /// <summary>
+        /// Delete a category from the database
+        /// </summary>
+        /// <returns></returns>
+        private async Task Delete()
+		{
+            // Get all existing categories
+            List<Category> allCategories = await uow.CategoryRepo.GetAllModelsAsync();
+
+
+            // If the category, that should be deleted, has any child-elements, the user is not allowed to delete it
+            if (allCategories.Any(c => c.ParentId == category.CategoryId))
+			{
+                errorMessage = "This element has one or more child-elements attached to it. Please delete all child-elements first in order to delete this element.";
+			}
+            // The category has no child-elements => delete it
+			else
+			{
+                await uow.CategoryRepo.DeleteModelAsync(Convert.ToInt32(CategoryID));
+                nav.NavigateTo("/category");
+			}
+		}
+
+
+
+        /// <summary>
+        /// Show an error message, if the form-submit was invalid
+        /// </summary>
         private async Task Error()
         {
             errorMessage = "Something went wrong. Please try again";
@@ -73,9 +140,39 @@ namespace DevShop.Pages.Views.Categories
 
 
         #region Side-Methods
+        /// <summary>
+        /// Set the parent-element
+        /// </summary>
         private void ChangeParentElem(ChangeEventArgs args)
         {
             parentID = Convert.ToInt32(args.Value);
+        }
+
+
+
+        /// <summary>
+        /// Get the HTML-Code for the TreeView
+        /// </summary>
+        private async void GenerateTreeView()
+        {
+            // List of all categories
+            List<Category> treeCategories = await uow.CategoryRepo.GetAllModelsAsync();
+
+            // Convert the list of categories to TreeView-elements
+            List<TreeViewElementVM> treeViewElements = treeCategories.Select(c => new TreeViewElementVM()
+            {
+                ElemID = c.CategoryId,
+                ElemText = c.CategoryName,
+                ParentID = c.ParentId,
+                ElemLink = "./category/edit/" + c.CategoryId.ToString()
+            }).ToList();
+
+
+            // Get the HTML-Code for the TreeView
+            treeViewMarkup = treeBuilder.BuildTree(treeViewElements);
+
+            // Update the data on the view
+            StateHasChanged();
         }
         #endregion
         #endregion
