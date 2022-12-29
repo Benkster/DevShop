be9fe3e7c9f43b85021fb00ba8b9789e2bc376f2
benni <benjamin.knoll.bk@gmail.com>
2022-12-29 16:58:55 +0100

    Product-Group View

---
+++ DevShop.App/DevShop/Data/Repos/IRepos/IArticleRepo.cs
@@ -0,0 +1,13 @@
+﻿using DevShop.Data.Models;
+
+namespace DevShop.Data.Repos.IRepos
+{
+	/// <summary>
+	/// Interface for Article-Repository.
+	/// Defines all methods, that are necessary for the repo
+	/// </summary>
+	public interface IArticleRepo
+	{
+		Task<List<Article>> GetAllModelsAsync(string _compCode);
+	}
+}
