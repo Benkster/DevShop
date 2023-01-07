e99206e78105d182615cd8a8b6b47b8054b6170c
benni <benjamin.knoll.bk@gmail.com>
2023-01-07 17:01:41 +0100

    Article-Header - Style

--- DevShop.App/DevShop/wwwroot/js/popup.js
+++ DevShop.App/DevShop/wwwroot/js/popup.js
@@ -1,37 +1,64 @@
-﻿function LightFrame(_url, _width, _height) {
-	var popupContent = '<iframe id="popupIframe" src="' + _url + '" width="' + _width + '" height="' + _height + '"></iframe>';
+﻿function LightFrame(_url, _width, _height, _reloadPage) {
+	_reloadPage = (_reloadPage) ? _reloadPage : false;
 
-	OpenLightFrame(popupContent);
+	var popupSettings = 'src="' + _url + '" width="' + _width + '" height="' + _height + '"';
+	var containerSettings = 'width: ' + _width + '; height: ' + _height + ';'	
+
+	OpenLightFrame(popupSettings, containerSettings, _reloadPage);
 }
 
 
 
-function LightFrame(_url, _width, _height, _maxWidth, _maxHeight) {
-	var popupContent = '<iframe id="popupIframe" src="' + _url + '" width="' + _width + '" height="' + _height + '" style="max-width: ' + _maxWidth + '; max-height: ' + _maxHeight + '"></iframe>';
+function LightFrame(_url, _width, _height, _maxWidth, _maxHeight, _reloadPage) {
+	_reloadPage = (_reloadPage) ? _reloadPage : false;
 
-	OpenLightFrame(popupContent);
+	var popupSettings = 'src="' + _url + '" width="' + _width + '" height="' + _height + '" style="max-width: ' + _maxWidth + '; max-height: ' + _maxHeight + '"';
+	var containerSettings = 'width: ' + _width + '; height: ' + _height + '; max-width: ' + _maxWidth + '; max-height: ' + _maxHeight + ';';
+
+	OpenLightFrame(popupSettings, containerSettings, _reloadPage);
 }
 
 
 
-function OpenLightFrame(_popupContent) {
+var allowClose = false;
+
+function OpenLightFrame(_popupSettings, _containerSettings, _reloadPage) {
+	allowClose = false;
+
 	const lfContainer = document.createElement('div');
+	var popupContent = '<iframe id="popupIframe" class="noLine" ' + _popupSettings + '></iframe>';
 
-	var closePopup = '<div id="closePopup"><a href="javascript:CloseLightFrame();">Close</a></div>'
+	var closePopup = '<div id="closePopup"><a href="javascript:CloseLightFrame(' + _reloadPage + ');"></a></div>'
 
 	lfContainer.id = 'popupContainer';
-	lfContainer.innerHTML = _popupContent + closePopup;
+	lfContainer.innerHTML = '<div id="popupIframeContainer" class="fixed centerPos backClearColor thickDarkGrayLine" style="' + _containerSettings + '">' + popupContent + closePopup + '</div>';
+	lfContainer.addEventListener('click', CloseLightFrame);
 
 	document.body.appendChild(lfContainer);
+
+	allowClose = true;
 }
 
 
 
-function CloseLightFrame() {
-	var lfContainer = document.getElementById('popupContainer');
-
-	if (lfContainer)
+function CloseLightFrame(_reloadPage) {
+	if (allowClose)
 	{
-		lfContainer.remove();
+		_reloadPage = (_reloadPage) ? _reloadPage : true;
+
+
+		var lfContainer = document.getElementById('popupContainer');
+
+		if (lfContainer)
+		{
+			lfContainer.remove();
+		}
+
+
+
+		if (_reloadPage) 
+		{
+			location.reload();
+		}
 	}
 }
